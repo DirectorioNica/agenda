89d9deb45ec2ed5d3b64939c97798a024453d7b2
DirectorioNica <ktcrsrwd25@gmail.com>
2024-06-27 02:24:52 +0200

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -1,23 +1,13 @@
 document.addEventListener('init', function(event) {
   var page = event.target;
 
-  // Lógica para la página 'page1.html'
   if (page.id === 'page1') {
     page.querySelector('#push-button').onclick = function() {
       document.querySelector('#myNavigator').pushPage('info.html');
     };
-
-    // ... más código para la página 'page1.html'
-  } 
-
-  // Lógica para la página 'page2.html'
-  else if (page.id === 'page2') {
+  } else if (page.id === 'info') {
     page.querySelector('#pop-button').onclick = function() {
       document.querySelector('#myNavigator').popPage();
     };
-
-    // ... más código para la página 'page2.html'
   }
-
-  // Lógica para otras páginas...
 });
