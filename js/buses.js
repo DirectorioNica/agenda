d7a2ca7aab7fcdb76bf24cf9fb31e1e2692a387a
DirectorioNica <ktcrsrwd25@gmail.com>
2024-06-27 04:15:31 +0200

    Update buses.js

--- js/buses.js
+++ js/buses.js
@@ -31,7 +31,7 @@ function renderBuses(buses) {
       <div class="right">
         <span class="list-item__label">Salida: ${bus.get('salida')}</span>
         <span class="list-item__label">Llegada: ${bus.get('llegada')}</span>
-        <span class="list-item__label">Tel: ${bus.get('telefono')}</span>
+        <a href="tel:${bus.get('telefono')}"><span class="list-item__label">Tel: ${bus.get('telefono')}</span></a>
       </div>
     `;
     listItem.innerHTML = busInfo;
